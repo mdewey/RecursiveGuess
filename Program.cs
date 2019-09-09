1fdbd8cb926da3c12b9ce5c8927309cedd331ae2
Mark Dewey <mark.t.dewey@gmail.com>
2019-09-09 16:53:17 -0400

    before pattern matching

--- Program.cs
+++ Program.cs
@@ -4,28 +4,45 @@ namespace RecursiveGuess
 {
   class Program
   {
-    static void Logic(string input)
+    static int MAX = 100;
+    static int MIN = 0;
+    static Func<int, int, int> CalculateGuess = (high, low) => (high + low) / 2;
+    static void Logic(string input, int guess, int high, int low)
     {
-      if (!String.IsNullOrWhiteSpace(input))
-      {
-        Console.WriteLine("you entered " + input);
-      }
-
-
+      Action<int> printGuess = g => Console.WriteLine($"Is your number {g}");
       if (input == "stop")
       {
         return;
       }
+      else if (input == "yes")
+      {
+        Console.WriteLine("Awesome!");
+      }
+      else if (input == "h")
+      {
+        low = guess;
+        guess = CalculateGuess(high, low);
+        printGuess(guess);
+        Logic(Console.ReadLine(), guess, high, low);
+      }
+      else if (input == "l")
+      {
+        high = guess;
+        guess = CalculateGuess(high, low);
+        printGuess(guess);
+        Logic(Console.ReadLine(), guess, high, low);
+      }
       else
       {
-        Logic(Console.ReadLine());
+        printGuess(guess);
+        Logic(Console.ReadLine(), guess, high, low);
       }
     }
 
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      Logic(String.Empty);
+      Logic(String.Empty, CalculateGuess(MAX, MIN), MAX, MIN);
     }
   }
 }
